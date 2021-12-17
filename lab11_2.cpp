c8577921c5d9e1043160fa0986f6118042d1377d
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-12-17 16:04:30 +0700

    lab11_2.cpp

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,9 +9,14 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	string texline;
+	dest << "-------------------- BOOM ---------------------" << "\n";
+	while(getline(source,texline))
+	{
+		dest << texline << '\n';
+	}
+	dest << "-------------------- HA!! ---------------------";
+	
     source.close();
     dest.close();
 	return 0;
